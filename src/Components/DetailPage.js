9be09c2d4d2ce4ec94ad297fc57ad17070ea1909
shaileshpandit141 <shaileshpandit141@gmail.com>
2023-11-06 02:00:08 +0530

    Update Detail Component

--- src/Components/DetailPage.js
+++ src/Components/DetailPage.js
@@ -1,8 +1,9 @@
 export default function DetailPage(data, id) {
+    let El = id > 9? id: `0${id}`
     return (
         `
         <div class="questions">
-            <p>${id}:</p>
+            <p>${El}:</p>
             <p>${data}</p>
         </div>
         `
